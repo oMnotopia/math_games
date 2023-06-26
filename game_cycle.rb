2edd89ce9bc20e6b0966661ab0e3ef98e5d1c21a
Clayton <persinger.clayton@gmail.com>
2023-06-26 11:15:46 -0700

    Update logic for changing turns

--- game_cycle.rb
+++ game_cycle.rb
@@ -7,26 +7,34 @@ require "./players"
 def game_cycle
   win_condition = WinCondition.new
   player_turn = PlayerTurn.new
-  player_lives = PlayerLives.new(1, 1)
+  player_lives = PlayerLives.new(3, 3)
 
   until (win_condition.did_someone_win(player_lives.player_1_lives, player_lives.player_2_lives))
-    turn = player_turn.get_turn
+    turn = player_turn.turn
     random_num = rand(1..20)
     puts "Player #{turn}: #{@math_questions[random_num][:question]}"
     player_answer = gets.chomp
     unless(@math_questions[random_num][:answer] == player_answer.to_i)
+      puts "Player #{turn}: Sorry, that's wrong!"
       if (turn == 1)
         player_lives.player_1_lives -= 1
       else
         player_lives.player_2_lives -= 1
       end
+    else
+      puts "Player #{turn}: Yes! You are correct."
     end
 
+    if(player_turn.turn == 1)
+      player_turn.turn = 2
+    else 
+      player_turn.turn = 1
+    end  
+      
     puts "P1: #{player_lives.player_1_lives}/3 vs P2: #{player_lives.player_2_lives}/3"
+    puts ""
     puts "----- NEW TURN -----"
   end
-  puts player_lives.player_1_lives, player_lives.player_2_lives
-
 end
 
 game_cycle
