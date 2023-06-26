62a1c70e4a4be9c861f591489865a35f81e3d931
Clayton <persinger.clayton@gmail.com>
2023-06-26 08:53:09 -0700

    Create 20 math questions with answers

---
+++ questions.rb
@@ -0,0 +1,86 @@
+# This class will contain and generate math questions.
+
+# Methods
+# 1. method to generate a math question from a list  of questions
+@math_questions = [
+  {
+    question: "What is 5 + 7?",
+    answer: 12
+  },
+  {
+    question: "Subtract 9 from 20.",
+    answer: 11
+  },
+  {
+    question: "Multiply 3 by 4.",
+    answer: 12
+  },
+  {
+    question: "Divide 36 by 6.",
+    answer: 6
+  },
+  {
+    question: "Calculate 8 squared.",
+    answer: 64
+  },
+  {
+    question: "What is 15 minus 9?",
+    answer: 6
+  },
+  {
+    question: "Add 2.5 and 3.2.",
+    answer: 5.7
+  },
+  {
+    question: "Multiply 5 by 10.",
+    answer: 50
+  },
+  {
+    question: "Divide 45 by 9.",
+    answer: 5
+  },
+  {
+    question: "What is 14 plus 18?",
+    answer: 32
+  },
+  {
+    question: "Subtract 4 from 11.",
+    answer: 7
+  },
+  {
+    question: "Multiply 6 by 7.",
+    answer: 42
+  },
+  {
+    question: "Divide 72 by 8.",
+    answer: 9
+  },
+  {
+    question: "Calculate 5 cubed.",
+    answer: 125
+  },
+  {
+    question: "What is 25 minus 17?",
+    answer: 8
+  },
+  {
+    question: "Add 3.7 and 2.4.",
+    answer: 6.1
+  },
+  {
+    question: "Multiply 8 by 9.",
+    answer: 72
+  },
+  {
+    question: "Divide 63 by 7.",
+    answer: 9
+  },
+  {
+    question: "What is 22 plus 14?",
+    answer: 36
+  },
+  {
+    question: "Subtract 8 from 15.",
+    answer: 7
+  }
+]
