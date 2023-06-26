2a5fa0c9348e50ae79d220a5fc30694e79d6dc45
Clayton <persinger.clayton@gmail.com>
2023-06-26 10:21:48 -0700

    Add PlayerTurn and PlayerLives class. Implement logic to determine turn and remove lives on wrong answer

--- game_cycle.rb
+++ game_cycle.rb
@@ -1,19 +1,31 @@
 # This class will loop through the game sequence and call other class methods as needed.
 require "./win_condition"
 require "./questions"
+require "./turn"
+require "./players"
 
 def game_cycle
   win_condition = WinCondition.new
-  lives1 = 1
-  lives2 = 1
-  until (win_condition.did_someone_win(lives1, lives2))
+  player_turn = PlayerTurn.new
+  player_lives = PlayerLives.new(1, 1)
+
+  until (win_condition.did_someone_win(player_lives.player_1_lives, player_lives.player_2_lives))
+    turn = player_turn.get_turn
     random_num = rand(1..20)
-    puts "#{@math_questions[random_num][:question]}"
+    puts "Player #{turn}: #{@math_questions[random_num][:question]}"
     player_answer = gets.chomp
     unless(@math_questions[random_num][:answer] == player_answer.to_i)
-      lives1 -= 1
+      if (turn == 1)
+        player_lives.player_1_lives -= 1
+      else
+        player_lives.player_2_lives -= 1
+      end
     end
+
+    puts "P1: #{player_lives.player_1_lives}/3 vs P2: #{player_lives.player_2_lives}/3"
+    puts "----- NEW TURN -----"
   end
+  puts player_lives.player_1_lives, player_lives.player_2_lives
 
 end
 
