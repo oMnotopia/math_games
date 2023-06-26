37d92d3e478e331e28b941a7d6eaa7075ae68e71
Clayton <persinger.clayton@gmail.com>
2023-06-26 11:21:45 -0700

    Update logic to remove PlayerTurn class and consolidate in Players

--- game_cycle.rb
+++ game_cycle.rb
@@ -6,32 +6,31 @@ require "./players"
 
 def game_cycle
   win_condition = WinCondition.new
-  player_turn = PlayerTurn.new
-  player_lives = PlayerLives.new(3, 3)
+  players = Players.new(3, 3)
 
-  until (win_condition.did_someone_win(player_lives.player_1_lives, player_lives.player_2_lives))
-    turn = player_turn.turn
+  until (win_condition.did_someone_win(players.player_1_lives, players.player_2_lives))
+    turn = players.turn
     random_num = rand(1..20)
     puts "Player #{turn}: #{@math_questions[random_num][:question]}"
     player_answer = gets.chomp
     unless(@math_questions[random_num][:answer] == player_answer.to_i)
       puts "Player #{turn}: Sorry, that's wrong!"
       if (turn == 1)
-        player_lives.player_1_lives -= 1
+        players.player_1_lives -= 1
       else
-        player_lives.player_2_lives -= 1
+        players.player_2_lives -= 1
       end
     else
       puts "Player #{turn}: Yes! You are correct."
     end
 
-    if(player_turn.turn == 1)
-      player_turn.turn = 2
+    if(players.turn == 1)
+      players.turn = 2
     else 
-      player_turn.turn = 1
+      players.turn = 1
     end  
       
-    puts "P1: #{player_lives.player_1_lives}/3 vs P2: #{player_lives.player_2_lives}/3"
+    puts "P1: #{players.player_1_lives}/3 vs P2: #{players.player_2_lives}/3"
     puts ""
     puts "----- NEW TURN -----"
   end
